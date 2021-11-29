43c102a37e0a01cb0ec700f295b32fe399d8440a
Bipratip Bhowmick Tirzak <bipratipbhowmick@gmail.com>
2021-11-29 16:28:16 -0500

    Add base64 auth

--- handleRequest.py
+++ handleRequest.py
@@ -1,16 +1,20 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+import config
 with open('tickets.json') as f:
   datas= json.load(f)
 
 URL="https://zcckry.zendesk.com/api/v2/tickets.json"
 URL2="https://zcckry.zendesk.com/api/v2/tickets/create_many"
-URL3="https://zcckry.zendesk.com/api/v2/job_statuses/82772a89b28a97ae57efbbdd8fd45dfc.json"
-head = {'Authorization': 'Basic email_address/token:api_token {}'.format()}
+base64Token="{}".format(config.email, config.password)
+head = {'Authorization': 'Basic '}
+print(head)
 def getTickets():
-    response=requests.get(url=URL3, auth=HTTPBasicAuth('', ''))
+    response=requests.get(url=URL, headers=head)
     print(response.json())
+
+
 
 
 
