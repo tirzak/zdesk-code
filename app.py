ed9899869cd076fbcfecd8bb7d81cf4e625e9ae1
Bipratip Bhowmick Tirzak <bipratipbhowmick@gmail.com>
2021-11-30 21:49:27 -0500

    Fix output display

--- app.py
+++ app.py
@@ -9,6 +9,24 @@ def Cyan(val):
 def Green(val): 
     print("\033[92m {}\033[00m" .format(val))
 
+def messages(parameter):
+    if parameter == 1:
+        Green("Page {}, only showing 25 results. Type 'n' to go to the next page. Or,".format(sg.getCurrPage()))
+    elif parameter == 2:
+         Green("Page {}, only showing 25 results. Type 'n' to go to the next page, or type 'p' to go to the previous page. Or,".format(sg.getCurrPage()))
+    elif parameter == 3:
+         Green("Page {}, Type 'p' to go to the previous page. Or,".format(sg.getCurrPage()))
+
+def handleOutput():
+    obj=sg.getHasMore()
+    if obj[0]==1 and sg.getCurrPage()==1:
+        messages(1)
+
+    elif obj[0]==0 and obj[2]!="":
+        messages(3)
+    else:
+        messages(2)
+
 
 
 def menuOptions():
@@ -22,15 +40,17 @@ def menuOptions():
     
 
     while menuAnswer != "quit":
+        obj=sg.getHasMore()
         if menuAnswer == "menu":
             print(menuValues)
             menuAnswer=input()
             continue
         elif menuAnswer =="1":
             asyncio.run(hR.getTickets(sg))
-            obj=sg.getHasMore()
-            if obj[0]==1:
-                Green("Only showing 25 results. Type 'n' to go to the next page. Or,")
+            booas=1
+            handleOutput()
+           
+
 
         elif menuAnswer=="2":
             Cyan("Please enter a ticket number:")
@@ -42,9 +62,14 @@ def menuOptions():
                 asyncio.run(hR.getOneTicket(ticketValue,sg))
         elif menuAnswer=="n" and obj[0]==1:
             asyncio.run(hR.getTickets(sg,obj[1]))
-            obj=sg.getHasMore()
-            if obj[0]==1:
-                Green("Only showing 25 results. Type 'n' to go to the next page. Or,")
+            sg.setCurrPage(sg.getCurrPage()+1)
+            handleOutput()
+
+        elif menuAnswer=="p" and obj[2]!="" and sg.getCurrPage()!=1:
+             asyncio.run(hR.getTickets(sg,obj[2]))
+             sg.setCurrPage(sg.getCurrPage()-1)
+             handleOutput()
+
         else:
             Red("Sorry! Could not understand the input")
 
