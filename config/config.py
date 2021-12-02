9dcadf4b022d7c645c4a2176d9d7a3c35a199623
Bipratip Bhowmick Tirzak <47579342+tirzak@users.noreply.github.com>
2021-12-02 00:12:42 -0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 #This file contains the credentials used by the application
-#Add your credentials inside the double quote. Use API Token
+#Add your credentials inside the double quotes. 
 subdomain="zcckry.zendesk.com"
 oauth2AccessToken="10b078b5e2791ab59d6fa96b15f5e0b95c57101b2d00a0e8aba5a562a8e7670a"
 
